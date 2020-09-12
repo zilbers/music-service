6d9bf2528c85c874467925dd21628ad44d58db0b
Omri Zilberstein <67857576+zilbers@users.noreply.github.com>
2020-09-12 19:26:06 +0300

    Added top API's

--- server/app.js
+++ server/app.js
@@ -31,8 +31,7 @@ mysqlCon.connect((err) => {
 });
 
 // Get all
-app.get('/api/:table', (req, res) => {
-  console.log(req.params.table);
+app.get('/api/lists/:table', (req, res) => {
   const { table } = req.params;
   mysqlCon.query(`SELECT * FROM ${table}`, (error, results, fields) => {
     if (error) {
@@ -43,7 +42,7 @@ app.get('/api/:table', (req, res) => {
 });
 
 // Get by ID
-app.get('/api/:table/:id', async (req, res) => {
+app.get('/api/lists/:table/:id', async (req, res) => {
   const { table, id } = req.params;
   mysqlCon.query(`SELECT * FROM ${table} WHERE ${table.slice(0, -1)}_id = ?`,
     [id], (error, results, fields) => {
@@ -53,4 +52,34 @@ app.get('/api/:table/:id', async (req, res) => {
       }
       results[0] ? res.send(results) : res.status(404).send(`No ${req.params.table} with this ID`);
     });
+});
+
+// Get the top liked songs
+app.get('/api/top/songs', (req, res) => {
+  mysqlCon.query(`SELECT user_liked_songs.song_id, songs.title, COUNT(user_liked_songs.song_id) AS likes
+  FROM music_service.user_liked_songs
+  JOIN music_service.songs ON user_liked_songs.song_id=songs.song_id
+  GROUP BY song_id
+  ORDER BY likes DESC
+  LIMIT 20`, (error, results, fields) => {
+    if (error) {
+      res.status(500).send(error.message);
+    }
+    res.send(results);
+  });
+});
+
+// Get the top liked playlists
+app.get('/api/top/playlists', (req, res) => {
+  mysqlCon.query(`SELECT playlists.playlist_id, playlists.name, COUNT(user_playlists.playlist_id) AS saves
+  FROM music_service.user_playlists
+  JOIN music_service.playlists ON playlists.playlist_id=user_playlists.playlist_id
+  GROUP BY playlist_id
+  ORDER BY saves DESC
+  LIMIT 20`, (error, results, fields) => {
+    if (error) {
+      res.status(500).send(error.message);
+    }
+    res.send(results);
+  });
 });
