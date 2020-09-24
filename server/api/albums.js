cb10d67e0a1aa803009293d1f2f4d6a47015d34c
Omri Zilberstein <67857576+zilbers@users.noreply.github.com>
2020-09-24 21:18:45 +0300

    Added nost API's to album API

--- server/api/albums.js
+++ server/api/albums.js
@@ -1,5 +1,9 @@
+/* eslint-disable camelcase */
 const { Router } = require('express');
-const { Album, Song, Artist } = require('../models');
+const { Sequelize } = require('sequelize');
+const {
+  Album, Song, Artist, User_album,
+} = require('../models');
 
 const albumRouter = Router();
 
@@ -8,8 +12,8 @@ albumRouter.get('/', async (req, res) => {
   try {
     const allAlbums = await Album.findAll();
     res.json(allAlbums);
-  } catch {
-    res.status(500).send('Error');
+  } catch (e) {
+    res.status(500).send(e.message);
   }
 });
 
@@ -48,16 +52,23 @@ albumRouter.get('/:id/list', async (req, res) => {
 // Get the top albums
 albumRouter.get('/top', async (req, res) => {
   try {
-    const allAlbums = await Album.findAll({
+    const allAlbums = await User_album.findAll({
+      attributes: [[Sequelize.fn('COUNT', Sequelize.col('user_id')), 'likes']],
       include: [
         {
-          model: Artist,
-          attributes: ['name'],
-        },
-        {
-          model: Song,
+          model: Album,
+          attributes: ['name', 'cover_img'],
+          include: {
+            model: Artist,
+            attributes: ['name'],
+          },
         },
       ],
+      order: [
+        ['album_id', 'ASC'],
+      ],
+      group: ['album_id'],
+      raw: true,
     });
     res.json(allAlbums);
   } catch (e) {
@@ -65,20 +76,27 @@ albumRouter.get('/top', async (req, res) => {
   }
 });
 
-// // Post new data to albums
-// albumRouter.post('/api/albums', (req, res) => {
-//   const collums = req.body.collums.map((collum) => `\`${collum}\``).join();
-//   const values = req.body.values.map((value) => `'${value}'`).join();
-//   const date = new Date().toISOString().slice(0, 19).replace('T', ' ');
+// Post new data to albums
+albumRouter.post('/', async (req, res) => {
+  const { values, collums } = req.body;
+  const query = {};
+  collums.forEach((collum, index) => { query[collum] = values[index]; });
+  query.date = new Date().toISOString().slice(0, 19).replace('T', ' ');
+  try {
+    await Album.create(query);
+    res.status(200).send('Posted new album');
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
 
-//   mysqlCon.query(`INSERT INTO \`albums\` (${collums}, uploaded_at)
-//       VALUES (${values}, '${date}')`, (error, results, fields) => {
-//     if (error) {
-//       return res.status(500).send(error.message);
-//     }
-//     res.status(200).send('Uploaded new album');
-//   });
-// });
+  // mysqlCon.query(`INSERT INTO \`albums\` (${collums}, uploaded_at)
+  //     VALUES (${values}, '${date}')`, (error, results, fields) => {
+  //   if (error) {
+  //     return res.status(500).send(error.message);
+  //   }
+  //   res.status(200).send('Uploaded new album');
+  // });
+});
 
 // // Update albums data in the database
 // albumRouter.put('/:id', (req, res) => {
