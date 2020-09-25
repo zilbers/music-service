68c0a60dea1084cbc008bc1255c54bc40f717803
Omri Zilberstein <67857576+zilbers@users.noreply.github.com>
2020-09-25 12:59:02 +0300

    Added search API

--- server/api/search.js
+++ server/api/search.js
@@ -1,20 +1,23 @@
 const { Router } = require('express');
+const { Op } = require('sequelize');
+const { Song } = require('../models');
 
 const searchRouter = Router();
 
-// searchRouter.get('/', (req, res) => {
-//   const { filter } = req.query;
-//   const query = `SELECT songs.song_id AS id, songs.title AS name, artists.name as artist, created_at, songs.youtube_link
-//     FROM music_service.songs
-//     JOIN artists ON songs.artist_id = artists.artist_id
-//     WHERE songs.title LIKE '${filter}%'`;
-//   mysqlCon.query(query,
-//     (error, results, fields) => {
-//       if (error) {
-//         return res.status(500).send(error.message);
-//       }
-//       return res.status(200).send(results);
-//     });
-// });
+searchRouter.get('/', async (req, res) => {
+  const { filter } = req.query;
+  try {
+    const filteredSongs = await Song.findAll({
+      where: {
+        name: {
+          [Op.like]: `${filter}%`,
+        },
+      },
+    });
+    res.json(filteredSongs);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
 
 module.exports = searchRouter;
