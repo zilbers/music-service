7ec73baf39a09117389faa776a209a2f525c343b
Omri Zilberstein <67857576+zilbers@users.noreply.github.com>
2020-10-28 17:47:16 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
-const express = require('express');
-require('dotenv').config();
+const express = require("express");
+require("dotenv").config();
 
 const app = express();
 module.exports = app;
@@ -7,7 +7,7 @@ let requestID = 0;
 
 function logger(req, res, next) {
   console.log(
-    `Request #${requestID}\nRequest fired: ${req.url}\nMethod: ${req.method}`,
+    `Request #${requestID}\nRequest fired: ${req.url}\nMethod: ${req.method}`
   );
   requestID += 1;
   next();
@@ -15,8 +15,6 @@ function logger(req, res, next) {
 
 app.use(logger);
 app.use(express.json());
-app.use('/', express.static('../client/build/'));
+app.use("/", express.static("../client/build/"));
 
-app.use('/api/', require('./api'));
-
-module.exports = app;
+app.use("/api/", require("./api"));
