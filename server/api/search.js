3fb21e5cedcd05f4b5c421324d292df3d4ab36a0
Omri Zilberstein <67857576+zilbers@users.noreply.github.com>
2020-11-23 00:11:09 +0200

    fixed hot reolading

--- server/api/search.js
+++ server/api/search.js
@@ -1,7 +1,8 @@
 const { Router } = require('express');
 const { Client } = require('@elastic/elasticsearch');
-const { Op } = require('sequelize');
-const { Song, Album, Playlist, Artist } = require('../models');
+// const { Op } = require('sequelize');
+// const { Song, Album, Playlist, Artist } = require('../models');
+const { Song } = require('../models');
 
 const client = new Client({
   cloud: {
@@ -13,58 +14,58 @@ const client = new Client({
   },
 });
 
-const songsProps = {
-  id: { type: 'integer' },
-  name: { type: 'text' },
-  artistId: { type: 'integer' },
-  youtubeLink: { type: 'text' },
-};
+// const songsProps = {
+//   id: { type: 'integer' },
+//   name: { type: 'text' },
+//   artistId: { type: 'integer' },
+//   youtubeLink: { type: 'text' },
+// };
 
-async function bulkCreate(index, data, properties) {
-  await client.indices.create(
-    {
-      index,
-      body: {
-        mappings: {
-          properties: {
-            ...properties,
-          },
-        },
-      },
-    },
-    { ignore: [400] }
-  );
+// async function bulkCreate(index, data, properties) {
+//   await client.indices.create(
+//     {
+//       index,
+//       body: {
+//         mappings: {
+//           properties: {
+//             ...properties,
+//           },
+//         },
+//       },
+//     },
+//     { ignore: [400] }
+//   );
 
-  const body = data.flatMap((doc) => [{ index: { _index: index } }, doc]);
+//   const body = data.flatMap((doc) => [{ index: { _index: index } }, doc]);
 
-  const { body: bulkResponse } = await client.bulk({ refresh: true, body });
+//   const { body: bulkResponse } = await client.bulk({ refresh: true, body });
 
-  if (bulkResponse.errors) {
-    const erroredDocuments = [];
-    // The items array has the same order of the dataset we just indexed.
-    // The presence of the `error` key indicates that the operation
-    // that we did for the document has failed.
-    bulkResponse.items.forEach((action, i) => {
-      const operation = Object.keys(action)[0];
-      if (action[operation].error) {
-        erroredDocuments.push({
-          // If the status is 429 it means that you can retry the document,
-          // otherwise it's very likely a mapping error, and you should
-          // fix the document before to try it again.
-          status: action[operation].status,
-          error: action[operation].error,
-          operation: body[i * 2],
-          document: body[i * 2 + 1],
-        });
-      }
-    });
-    console.log(erroredDocuments);
-  }
+//   if (bulkResponse.errors) {
+//     const erroredDocuments = [];
+//     // The items array has the same order of the dataset we just indexed.
+//     // The presence of the `error` key indicates that the operation
+//     // that we did for the document has failed.
+//     bulkResponse.items.forEach((action, i) => {
+//       const operation = Object.keys(action)[0];
+//       if (action[operation].error) {
+//         erroredDocuments.push({
+//           // If the status is 429 it means that you can retry the document,
+//           // otherwise it's very likely a mapping error, and you should
+//           // fix the document before to try it again.
+//           status: action[operation].status,
+//           error: action[operation].error,
+//           operation: body[i * 2],
+//           document: body[i * 2 + 1],
+//         });
+//       }
+//     });
+//     console.log(erroredDocuments);
+//   }
 
-  const { body: count } = await client.count({ index });
-  console.log(count);
-  return bulkResponse;
-}
+//   const { body: count } = await client.count({ index });
+//   console.log(count);
+//   return bulkResponse;
+// }
 
 const searchRouter = Router();
 
@@ -111,15 +112,15 @@ searchRouter.get('/', async (req, res) => {
   // }
 });
 
-searchRouter.get('/test', async (req, res) => {
-  const songs = await Song.findAll({
-    attributes: ['id', 'name', 'artistId', 'youtubeLink'],
-  });
-  const resp = await bulkCreate('songs', songs, songsProps);
-  res.json(resp);
+// searchRouter.get('/test', async (req, res) => {
+//   const songs = await Song.findAll({
+//     attributes: ['id', 'name', 'artistId', 'youtubeLink'],
+//   });
+//   const resp = await bulkCreate('songs', songs, songsProps);
+//   res.json(resp);
 
-  // const songsJSON = JSON.stringify(songs);
-  // const resp = await bulkCreate('song', JSON.parse(songsJSON), sognsProps);
-});
+//   // const songsJSON = JSON.stringify(songs);
+//   // const resp = await bulkCreate('song', JSON.parse(songsJSON), sognsProps);
+// });
 
 module.exports = searchRouter;
