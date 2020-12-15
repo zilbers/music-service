34864256111b2bbb2a20a8ca01abe0a54763e601
Omri Zilberstein <67857576+zilbers@users.noreply.github.com>
2020-12-15 10:32:28 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,4 +2,7 @@ const app = require('./app');
 require('dotenv').config();
 
 const { PORT } = process.env;
-app.listen(PORT, console.log(`Music Service is running at http://localhost:${PORT}`));
+app.listen(
+  PORT,
+  console.log(`Music Service is running at http://localhost:${PORT}`)
+);
